6514d95bd06c6442af3557648b01b6f6cb25efa4
ElKirito2314 <wellington.nunes2314@gmail.com>
2024-10-22 15:57:17 -0300

    testando

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 
 function HeartClick() {
-    
+    console.log('teste');
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<HeartClick />);
